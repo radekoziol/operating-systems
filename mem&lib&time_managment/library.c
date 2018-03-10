8f82d6420ec1856fd053b7399fbe63bd0b72d6c3
Radek Kozioł <radekoziol.it@gmail.com>
2018-03-10 18:31:39 +0100

    Added all functions to dynamically allocated arrays

--- mem&lib&time_managment/library.c
+++ mem&lib&time_managment/library.c
@@ -2,22 +2,26 @@
 
 #include <stdio.h>
 #include <malloc.h>
-#include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 
 struct Block{
 
-    //Actual block
-    char**          block;
+    //Actual string
+    char *       string;
     //Size
-    unsigned int     x,y;
+    unsigned int size;
 
 };
 
-struct Block * create_array(int size){
+struct Block * create_array(){
 
-    struct Block* blocks = calloc((size_t) size, sizeof *blocks);
+    struct Block* blocks = malloc(1 * sizeof *(blocks));
+
+    // On first element we store size of array blocks
+    blocks[0].size = (unsigned int) 1;
+
     return blocks;
 }
 
@@ -25,96 +29,158 @@ void delete_array(struct Block * array){
     free(array);
 }
 
-struct Block * add_block(struct Block * array, int *size_p, struct Block block){
+void add_block(struct Block ** array, struct Block block){
 
+    // If string is empty it is not added
+    if(block.string == NULL)
+        return;
+
+    // Iterating over array to find any empty cell
     int it = 0;
 
-    while(it < *size_p){
+    struct Block * temp_array = *array;
+    // Allocated size of array
+    int size = temp_array->size;
 
+    while(it < size){
         //Because calloc initializes so, it consider this as empty
-        if(array[it].x == 0){
-            array[it] = block;
+        if(temp_array[it].size == 0){
+            temp_array[it] = block;
             break;
         } else
             it++;
     }
 
-    // Array overflow - 2 * size
-    if(it == *size_p){
-        array = realloc(array, sizeof * array );
-        *size_p = *size_p * 2;
-        array[it] = block;
+    // Array overflow - we need realloc 2 * size
+    if(it == size){
+
+        struct Block* blocks  = (struct Block*)realloc(*array, (2 * size * sizeof(struct Block)));
+
+        if (blocks == NULL)
+            printf("Cannot allocate more memory.\n");
+        else {
+            blocks[it] = block;
+            blocks->size = (unsigned int) (size * 2);
+            *array = blocks;
+
+        }
+
     }
 
-    return array;
+}
+
+struct Block closest_block_sum_to(int x, struct Block * array){
+
+    /*
+        Idea is to calculate sum of each block and compare to closest sum we found earlier
+     */
+    int size = array->size;
+    int i,j;
+    struct Block closest_block;
+    // Any block has >0 chars, so to make loop work
+    int closest_block_sum = -1;
+
+    for(i = 1; i < size; i++){
+        int sum = 0;
+        for(j = 0; j < array[i].size; j++){
+            sum += array[i].string[j];
+        }
+
+        // Comparing length to given number
+        if(abs(closest_block_sum - x) > abs(sum - x) ){
+            closest_block_sum = sum;
+            closest_block = array[i];
+        }
+
+    }
+
+    return closest_block;
 }
 
 
-bool are_equal_blocks(struct Block block1, const int size1, struct Block block2, const int size2) {
+bool are_equal_blocks(struct Block block1, struct Block block2) {
 
-    if((block1.x != block2.x) || (block1.y != block2.y) )
+    /*
+        If blocks differ in size then false
+        else we check each char
+     */
+    if(block1.size != block2.size)
         return false;
     else
-        for(int i = 0; i < size1; i++)
-            for(int j = 0; j < size2; j++)
-                for(int k = 0; k < size2; k++)
-                    if(block1.block[i][k] != block2.block[j][k])
-                        return false;
-
+        for(int i = 0; i < block1.size; i++) {
+            if (block1.string[i] != block2.string[i])
+                return false;
+        }
 
     return true;
 }
 
 
-struct Block * delete_block(struct Block * array, const int *size_a, struct Block * blocks, int size_b){
+void delete_block(struct Block ** array, struct Block * blocks){
 
+    struct Block * temp_array = *array;
+    // Size of allocated array
+    int size = temp_array[0].size;
 
-    for(int i = 0; i < *size_a; i++){
-        for(int j = 0; j < size_b; j++){
-            if(are_equal_blocks(array[i],*size_a,blocks[j], size_b))
-                array[i].x = 0;
+    // We check each block with to_delete_array blocks [o(n^2)]
+    for(int i = 1; i < size; i++){
+        for(int j = 1; j < blocks->size; j++){
+            if(are_equal_blocks(temp_array[i],blocks[j]))
+                temp_array[i].size = 0;
+
         }
     }
 
-    return array;
+    *array = temp_array;
 }
 
 
 
 void hello(void) {
 
-    int size = 1;
 
     struct Block ex1;
-    ex1.x = 1;
-    ex1.y = 2;
-
-    char **arr = (char **)calloc((size_t) ex1.x, sizeof(char *));
-    arr[0] = (char *)calloc((size_t) ex1.y, sizeof(char));
-    arr[0][0] = 'x';
-//    for (int i=0; i< ex1.x; i++)
-//        arr[i] = (char *)calloc((size_t) ex1.y, sizeof(char));
-//
-//    for (int i = 0; i <  ex1.x; i++)
-//        for (int j = 0; j < ex1.y; j++)
-//            arr[i][j] = 'x';
-//
-//
-//    ex1.block = arr;
-
-    struct Block * blocks = create_array(size );
-
-    int* size_p = &size;
-    blocks = add_block(blocks, size_p, ex1);
-
     struct Block ex2;
-    ex2.y = 3;
+    struct Block ex3;
 
-    blocks = add_block(blocks, size_p, ex2);
+    ex1.size = 2;
+    ex1.string = (char *)calloc((size_t) ex1.size, sizeof(char *));
+
+    ex1.string[0] = 'a';
+    ex1.string[1] = 'a';
+
+    ex2.size = 2;
+    ex2.string = (char *)calloc((size_t) ex2.size, sizeof(char *));
+
+    ex2.string[0] = 'b';
+    ex2.string[1] = 'b';
+
+    ex3.size = 100;
+    ex3.string = (char *)calloc((size_t) ex3.size, sizeof(char *));
+
+    ex3.string[0] = 'c';
+    ex3.string[1] = 'c';
+
+    struct Block * blocks = create_array();
 
 
-    printf("%d\n", blocks[1].y);
+    add_block(&blocks, ex1);
+    add_block(&blocks, ex2);
+    add_block(&blocks, ex2);
+    add_block(&blocks, ex3);
+    add_block(&blocks, ex3);
+    add_block(&blocks, ex3);
+
+
+    struct Block block2 = closest_block_sum_to(197,blocks);
+
+    printf("%c", block2.string[1]);
+
+
 
     delete_array(blocks);
+
+
 }
+
 
