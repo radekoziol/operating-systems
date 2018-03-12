921956ed777cd3e852382140fc2505204a9dd81c
Radek Kozioł <radekoziol.it@gmail.com>
2018-03-12 17:57:22 +0100

    Possibilty of adding any number of time measurment tests added

--- mem&lib&time_managment/test (ex 2)/test.c
+++ mem&lib&time_managment/test (ex 2)/test.c
@@ -10,12 +10,8 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-void run_time_test(void (*f)(), char message [50] ){
 
-
-    printf("***********************\n");
-    printf(message);
-    printf("\n");
+double * run_time_test(void (*f)()){
 
     struct rusage usage;
     struct timeval user_start, user_end, system_start, system_end;
@@ -36,32 +32,126 @@ void run_time_test(void (*f)(), char message [50] ){
     char real_time[10];
     snprintf(real_time, 10, "%f", real_time_m);
 
-    printf("Real time : ");
-    printf(real_time);
-    printf("\n");
+    char * system_time = parse_to_string(system_start,system_end);
 
     char * user_time = parse_to_string(user_start,user_end);
 
-    int i;
+    double * result = calloc(3, sizeof(double));
+    result[0] = real_time_m;
+    result[1] = atof(system_time);
+    result[2] = atof(user_time);
 
+    return result;
+
+}
+
+void print_average_time(int test_number, double results [9][3]) {
+
+    printf("After %d tests, the results are:\n\n", test_number);
+
+    printf("Creating array dynamically time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[0][0]/test_number);
+    printf("\n");
     printf("User time : ");
-    for(i = 0; i < 10; i++) {
-        if(*(user_time + i) != NULL)
-            printf("%c", *(user_time + i));
-    }
+    printf("%f", results[0][1]/test_number);
     printf("\n");
-
-    char * system_time = parse_to_string(system_start,system_end);
-
     printf("System time : ");
-    for(i = 0; i < 10; i++) {
-        if(*(user_time + i) != NULL)
-            printf("%c", *(system_time + i));
-    }
+    printf("%f", results[0][2]/test_number);
+    printf("\n\n");
+
+    printf("Adding blocks dynamically array time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[1][0]/test_number);
     printf("\n");
+    printf("User time : ");
+    printf("%f", results[1][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[1][2]/test_number);
+    printf("\n\n");
+
+    printf("Adding blocks statically time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[2][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[2][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[2][2]/test_number);
+    printf("\n\n");
+
+    printf("Deleting blocks dynamically array time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[3][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[3][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[3][2]/test_number);
+    printf("\n\n");
+
+    printf("Deleting blocks statically array time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[4][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[4][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[4][2]/test_number);
+    printf("\n\n");
+
+    printf("Finding (dynamic array) closet sum to given argument time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[5][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[5][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[5][2]/test_number);
+    printf("\n\n");
+
+    printf("Finding (static array) closet sum to given argument time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[6][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[6][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[6][2]/test_number);
+    printf("\n\n");
+
+    printf("Adding, deleting and adding again blocks (dynamic array) time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[7][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[7][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[7][2]/test_number);
+    printf("\n\n");
+
+    printf("Adding, deleting and adding again blocks (static array) time measurement \n");
+    printf("Real time : ");
+    printf("%f", results[8][0]/test_number);
+    printf("\n");
+    printf("User time : ");
+    printf("%f", results[8][1]/test_number);
+    printf("\n");
+    printf("System time : ");
+    printf("%f", results[8][2]/test_number);
+    printf("\n\n");
 
 
 }
+
+
 
 void test(){
 
@@ -85,35 +175,84 @@ void test(){
      */
     char operations [3] = {'c','b','d'};
 
-    run_time_test(create_array_d_test,
-                  "Creating array dynamically time measurement");
+    int test_number = 1000;
+    double results [9][3];
+
+    int i,j;
+    for(i = 0; i < 9; i++) {
+        for (j = 0; j < 3; j++) {
+            results[i][j] = 0.0;
+        }
+    }
+
+    for(i = 0; i < test_number; i++) {
 
 
-    run_time_test(add_blocks_d_test,
-                  "Adding blocks dynamically array time measurement");
-
-    run_time_test(add_blocks_s_test,
-                  "Creating array statically time measurement");
-
-    run_time_test(delete_blocks_d_test,
-                  "Deleting blocks dynamically array time measurement");
-
-    run_time_test(delete_blocks_s_test,
-                  "Deleting blocks statically array time measurement");
-
-    run_time_test(closest_block_sum_to_d_test,
-                  "Finding (dynamic array) closet sum to given argument time measurement");
-
-    run_time_test(closest_block_sum_to_s_test,
-                  "Finding (static array) closet sum to given argument time measurement");
-
-    run_time_test(re_adding_to_array_d_test,
-                  "Adding, deleting and adding again blocks (dynamic array) time measurement");
-
-    run_time_test(re_adding_to_array_s_test,
-                  "Adding, deleting and adding again blocks (static array) time measurement");
+        //"Creating array dynamically time measurement"
+        double * result = run_time_test(create_array_d_test);
+        for(j = 0; j < 3; j++){
+            results[0][j] += result[j];
+        }
 
 
+        //"Adding blocks dynamically array time measurement"
+        result = run_time_test(add_blocks_d_test);
+
+        for(j = 0; j < 3; j++){
+            results[1][j] += result[j];
+        }
+
+        //"Adding blocks  statically time measurement"
+        result = run_time_test(add_blocks_s_test);
+
+        for(j = 0; j < 3; j++){
+            results[2][j] += result[j];
+        }
+
+        //"Deleting blocks dynamically array time measurement"
+        result = run_time_test(delete_blocks_d_test);
+
+        for(j = 0; j < 3; j++){
+            results[3][j] += result[j];
+        }
+
+        //"Deleting blocks statically array time measurement"
+        result = run_time_test(delete_blocks_s_test);
+
+        for(j = 0; j < 3; j++){
+            results[4][j] += result[j];
+        }
+
+        //"Finding (dynamic array) closet sum to given argument time measurement"
+        result = run_time_test(closest_block_sum_to_d_test);
+
+        for(j = 0; j < 3; j++){
+            results[5][j] += result[j];
+        }
+
+        //"Finding (static array) closet sum to given argument time measurement"
+        result = run_time_test(closest_block_sum_to_s_test);
+
+        for(j = 0; j < 3; j++){
+            results[6][j] += result[j];
+        }
+
+        //"Adding, deleting and adding again blocks (dynamic array) time measurement"
+        result = run_time_test(re_adding_to_array_d_test);
+
+        for(j = 0; j < 3; j++){
+            results[7][j] += result[j];
+        }
+
+        //"Adding, deleting and adding again blocks (static array) time measurement"
+        result = run_time_test(re_adding_to_array_s_test);
+
+        for(j = 0; j < 3; j++){
+            results[8][j] += result[j];
+        }
+    }
+
+    print_average_time(test_number, results);
 
 
 
