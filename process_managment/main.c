27f223860f157e720caa3e7d8d9b141a414da61c
Radek Kozioł <radekoziol.it@gmail.com>
2018-03-26 20:26:46 +0200

    Basic input control added

--- process_managment/main.c
+++ process_managment/main.c
@@ -18,51 +18,26 @@
 int
 main(int argc, char **argv) {
 
-    int time_l = 100;
-    int mem_l = 20;
+
+    if ((argc == 1) || strcmp(argv[1], "-help") == 0) {
+        printf("Arguments are: \n"
+                       "   path time_limit memory_limit \n");
+
+    }
+
+//    Example input
+    argv[0] = "../file.txt";
+    argv[1] = "10";
+    argv[2] = "100";
+
+
+    int time_l = (int) strtol(argv[1], NULL, 10);
+    int mem_l = (int) strtol(argv[2], NULL, 10);
     mem_l = 1048576*mem_l;
 
-    char* path= "../file.txt";
+    char* path= argv[0];
 
     execute_file(path,time_l,mem_l);
-
-//    if ((argc == 1) || strcmp(argv[1], "-help") == 0) {
-//        printf("Arguments are: \n"
-//                       "   path condition date (how)\n");
-//        printf("Description: \n");
-//        printf(" Possible conditions: \n"
-//                       "  '>' modified later than <date> \n"
-//                       "  '=' modified at the same <date> \n"
-//                       "  '<' modified earlier than <date> \n");
-//        printf(" Path must be in format day mm//dd/yy hh:mm:ss AM/PM "
-//                       "f.e 'Tue 10/30/2001 10:59:10 AM'\n");
-//        printf(" <how = 'sys'> (default) uses methods like opendir, ..\n"
-//                       " <how = 'nftw' uses method nftw\n");
-//        exit(0);
-//    }
-//    if (argc < 3) {
-//        printf("You did not input enough arguments!\nFor more information write -help\n");
-//        exit(0);
-//    }
-//
-//    time_t input_date = parse_date(argv[3]);
-//
-//    char condition = argv[2][0];
-//
-//    switch (condition) {
-//        case '>':
-//            list_files
-//                    (argv[1], is_later, input_date);
-//        case '<':
-//            list_files
-//                    (argv[1], is_earlier, input_date);
-//        case '=':
-//            list_files
-//                    (argv[1], is_equal, input_date);
-//
-//        default:
-//            break;
-//    }
 
 
     return 0;
