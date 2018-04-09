32d69ae94d5f02514603154d4e02090bacd43676
Radek Kozioł <radekoziol.it@gmail.com>
2018-04-09 20:04:02 +0200

    Sending signals works properly!

--- signal_managment/ex2/main.c
+++ signal_managment/ex2/main.c
@@ -3,54 +3,93 @@
 #include <signal.h>
 #include <wait.h>
 #include <stdbool.h>
+#include <memory.h>
 
-int k = 0;
-int child_pid[10];
-bool can_run = false;
+#define MAX_CHILD  100
 
+int child_pid[MAX_CHILD];
+int child_status[MAX_CHILD];
+int child_work[MAX_CHILD];
+// Parent allowance for children to execute
+bool children_can_run = false;
+
+volatile sig_atomic_t request_num = 0;
+
+int work();
 
 void sig_usr2(int signo) {
-    printf("Executing!\n");
-    if (signo == SIGUSR2)
-        sleep(1);
+    char buffer[100];
+    sprintf(buffer, "[CHILD] SIGUSR2 received from parent. Executing!\n");
+    write(1, buffer, strlen(buffer));
+
 }
 
-void sig_usr1(int signo) {
-    if (signo == SIGUSR1)
-        k++;
+void sig_usr1(int signo, siginfo_t *info, void *extra) {
+    request_num++;
+    char buffer[100];
+    sprintf(buffer, "[PARENT] SIGUSR1 received from %d \n", info->si_value.sival_int);
+    write(1, buffer, strlen(buffer));
 }
 
 
 int main() {
 
     int N = 10;
+    int M = 5;
     for (int j = 0; j < N; j++)
         child_pid[j] = 123;
     int c = 0;
 
-    pid_t pid, ppid;
+    pid_t pid, ppid,wpid;
     ppid = getpid();
 
 
-    if (signal(SIGUSR1, sig_usr1) == SIG_ERR)
-        printf("Something went wrong!\n");
+    struct sigaction action;
+
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = &sig_usr1;
+
+    if (sigaction(SIGUSR1, &action, NULL) == -1) {
+        perror("sigusr: sigaction");
+        return 0;
+    }
 
 
     for (int i = 0; i < N; ++i) {
+        int status;
+
         if ((child_pid[i] = fork()) == 0) {
+
             if (signal(SIGUSR2, sig_usr2) == SIG_ERR)
                 printf("Something went wrong!\n");
-            printf("Sending sigusr1 to parent!\n");
-            kill(ppid, SIGUSR1);
-            pause();
-        }else{
-            if((i>3)  || (can_run)){
-                printf("Allowing kids to execute!\n");
-                can_run = true;
-                for (int j = 0; j < 10; j++){
-                    kill(child_pid[j],SIGUSR2);
-                }
+            if(!children_can_run){
+
+                union sigval r_val;
+                r_val.sival_int = getpid();
+                sigqueue(ppid, SIGUSR1, r_val);
+
+                pause();
+                sleep(1);
+
+            }else{
+                sleep(1);
+//                int time = work();
             }
+
+            _exit(0);
+        }else{
+            int status;
+            wait(&status);
+
+            if((request_num>=M)  && (!children_can_run)){
+                children_can_run = true;
+                for (int j = 0; j < i; j++){
+                    kill(child_pid[j],SIGUSR2);
+                    wait(&status);
+                }
+
+            }
+
         }
     }
 
@@ -58,6 +97,8 @@ int main() {
     if (getpid() == ppid){
         for (int j = 0; j < N; j++)
             printf("%d,", child_pid[j]);
+
+        printf("\n%d\n", request_num);
     }
 
 
@@ -66,3 +107,11 @@ int main() {
 
 
 }
+
+//int work() {
+//    sprintf("start sleep\n");
+//    fflush(stdout);
+//    sleep(5);   //sleep 1s
+//    printf("stop sleep\n");
+//    return 0;
+//}
---
+++ signal_managment/ex2/utils.c
@@ -0,0 +1,43 @@
+//
+// Created by radekkoziol on 09.04.18.
+//
+
+#include <zconf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int get_random_signal() {
+    return 0;
+}
+
+int work(int rand_time) {
+
+    sleep(rand_time);
+    return rand_time;
+}
+
+// Sets global arrays to default
+void set_arr(int size, int child_pid[], int child_status[], int child_work[]) {
+    for (int j = 0; j < size; j++) {
+        child_status[j] = 0;
+        child_pid[j] = -10;
+        child_work[j] = 0;
+    }
+}
+
+void print_info(int size, int child_pid[], int child_status[],int child_work[]) {
+
+    printf("\n**********************************\n");
+
+    for (int j = 0; j < size; j++){
+        printf("Process %d:\n"
+                       " - executed with status %d\n"
+                       " - worked for %d sec\n",
+               child_pid[j], child_status[j],child_work[j]);
+    }
+
+
+    printf("\n**********************************\n");
+
+}
