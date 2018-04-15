f7be83b5fe28df8715f8b80f0c8f9369cbcb02b9
Radek Kozioł <radekoziol.it@gmail.com>
2018-04-15 16:11:49 +0200

    Old code adjusted to program

--- piepelines/library/utils.c
+++ piepelines/library/utils.c
@@ -8,125 +8,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/resource.h>
-#include <wait.h>
 #include <stdbool.h>
-#include "time_m.c"
+#include <signal.h>
+#include <wait.h>
 
 void
-set_limits( int time_limit, int mem_limit) {
+set_limits(int time_limit, int mem_limit) {
 
     // Some conditions
-    if((time_limit <= 0) || (mem_limit <= 0)){
+    if ((time_limit <= 0) || (mem_limit <= 0)) {
         printf("Those limits: ");
-        printf("time: %d, memory: %d", time_limit,mem_limit);
+        printf("time: %d, memory: %d", time_limit, mem_limit);
         printf("are impossible!");
         exit(-1);
     }
     struct rlimit lim;
 
     lim.rlim_cur = (rlim_t) time_limit;
-    setrlimit (RLIMIT_CPU, &lim);
+    setrlimit(RLIMIT_CPU, &lim);
 
     lim.rlim_cur = (rlim_t) mem_limit;
-    setrlimit (RLIMIT_AS, &lim);
+    setrlimit(RLIMIT_AS, &lim);
 
 }
 
+
+void print_info(char commands[256][256], char *args[256]) {
+    // Printing file info
+    printf("Executing");
+    int c = 0;
+    while (commands[c][0] != '\0') {
+        printf(" %s", commands[c]);
+        printf(" %s |", args[c]);
+        c++;
+    }
+}
+
+
 void
-execute_command(char *command, char *args[256], int time_limit, int mem_limit){
+execute_pipe(char command[256][256], char *args[256], int time_limit, int mem_limit){
+
+    print_info(command,args);
+
 
     pid_t child_pid;
     int status, command_s;
     status = command_s = 0;
 
-    // Creating new process
-    int pipes[2];
-    pipe(pipes);
-    child_pid = vfork();
-
-    if( 0 == child_pid ){
-
-        // Setting limits
-        set_limits(time_limit,mem_limit);
-
-        if(command[0] == '`')
-            _exit(127);
-                    
-        // Running command
-        if(args[1] == NULL)
-            command_s = execlp(command, command, (char *) NULL);
-        else
-            command_s = execvp(command, args);
-
-        if(command_s != 0){
-            printf("Process went wrong!\n");
-            kill(0, SIGTERM);
-        }
-        _exit(127); /* terminate the child  */
-    }
-
-    // this way, the father waits for all the child processes
-    while ( wait(&status) > 0 );
-
-    print_results(measure_time());
+//    // Creating new process
+//    int pipes[2];
+//    pipe(pipes);
+//    child_pid = vfork();
+//
+//    if( 0 == child_pid ){
+//
+//        // Setting limits
+//        set_limits(time_limit,mem_limit);
+//
+//        if(command[0] == '`')
+//            _exit(127);
+//
+//
+//        // Running command
+//        if(args[1] == NULL)
+//            command_s = execlp(command, command, (char *) NULL);
+//        else
+//            command_s = execvp(command, args);
+//
+//        if(command_s != 0){
+//            printf("Process went wrong!\n");
+//            kill(0, SIGTERM);
+//        }
+//        _exit(127); /* terminate the child  */
+//    }
+//
+//    // this way, the father waits for all the child processes
+//    while ( wait(&status) > 0 );
 
 }
 
-void
-parse_line(FILE *f, char *command, char *args[256]){
+int
+parse_line(const char file[256], int i, char *command, char args[256]) {
 
-    char file[256];
-    int i,j,k;
+    int j, k;
 
-    for(i = 0; i < 256; i++)
-        file[i] = '\0';
+    // Assuming next char is '|'
+    while (file[i] == ' ') i++;
 
-    // Reading line
-    fscanf(f, "%[^\n]", file);
-
+    k = 0;
     // Reading command
-    i = 0;
     while ((file[i] != ' ') && (file[i] != '\0')) {
-        command[i] = file[i];
-        i++;
+        command[k++] = file[i++];
     }
-    // Has to end with NULL
-    command[i] = '\0';
-    // According to execvp syntax
-    args[0] = command;
 
-    if(file[i] == '\0'){
-        args[1] = '\0';
-        return;
+    // Has to end with NULL
+    command[k] = '\0';
+
+    if (file[i] == '\0') {
+        args[k] = '\0';
+        return 0;
     }
 
     i++;
-    j = 1;
-    k = 0;
+    j = 0;
 
     // Now we need to split arguments into args array
-    while(file[i] != '\0'){
-
-        if(file[i] != ' ') {
-            args[j][k] = file[i];
-            k++;
-        }
-        else{
-            args[j][k] = '\0';
-            k = 0;
-            j++;
+    while ((file[i] != '\0') && (file[i] != '|')) {
+        if (file[i] != ' ') {
+            args[j++] = file[i];
         }
         i++;
     }
-
     // Last argument
-    args[j][k] = '\0';
-    args[j+1] = NULL;
+    args[j + 1] = '\0';
+
+    //
+    if (file[i] == '|') return i + 1;
+    return 0;
 
 }
 
 void
-execute_file(char * path, int time_lim, int mem_lim){
+execute_file(char *path, int time_lim, int mem_lim) {
 
     // Opening file
     FILE *f;
@@ -138,36 +141,37 @@ execute_file(char * path, int time_lim, int mem_lim){
     }
 
     // Command
-    char command[256];
+    char commands[256][256];
     // Arguments
     char *args[256];
 
-    for (int i=0; i<256; i++)
-        args[i] = (char *)malloc(256 * sizeof(char));
+    for (int i = 0; i < 256; i++) {
+        args[i] = (char *) malloc(256 * sizeof(char));
+    }
 
     // Iterating over each line in file
     // @TODO this loop probably can be done without this bool
     bool beg_char = true;
 
     // When checking we move pointer to next char - so we need to check 1st time
-    while (getc(f) != EOF){
+    while (getc(f) != EOF) {
 
-        if(beg_char)
-            fseek(f,-1,SEEK_CUR);
+        if (beg_char)
+            fseek(f, -1, SEEK_CUR);
+
+        // Reading line
+        char line[256];
+        fscanf(f, "%[^\n]", line);
 
         // Parse line arguments
-        parse_line(f,command,args);
+        int p_n = 0, x = 0;
+        while ((x = parse_line(line, x, commands[p_n++], args[p_n])) > 0);
+        commands[p_n][0] = '\0';
 
-        // Printing file info
-        printf("Executing ");
-        int i = 0;
-        while(args[i] != NULL) {
-            printf(" %s", args[i++]);
-        }
-        printf("\n");
+
 
         // Executing command
-        execute_command(command,args,time_lim,mem_lim);
+        execute_pipe(commands,args,time_lim,mem_lim);
 
         beg_char = false;
     }
--- piepelines/main.c
+++ piepelines/main.c
@@ -1,9 +1,21 @@
 //
-// Created by radekkoziol on 10.04.18.
+// Created by radekkoziol on 18.03.18.
 //
+
+#if defined(__linux__) || defined(__MINT__)
+# define _GNU_SOURCE /* strptime(), getsubopt() */
+
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "library/utils.c"
+
 
 #include <zconf.h>
 #include <stdio.h>
+#include <memory.h>
+
 /*
  * ps aux | grep root | wc -l
  */
@@ -40,14 +52,34 @@ void some_command(){
 
 }
 
-int main() {
-//
-//    parent_to_child_and_otherwayaround();
 
-    some_command();
+int
+main(int argc, char **argv) {
 
-    //    printf("child_to_parent\n");
-//    child_to_parent();
 
+
+//    Example input
+
+    argv[1] = "../file.txt";
+    argv[2] = "10";
+    argv[3] = "100";
+    argc = 3;
+
+    if ((argc == 1) || strcmp(argv[1], "-help") == 0) {
+        printf("Arguments are: \n"
+               "   path time_limit memory_limit \n");
+        exit(0);
+    }
+
+    int time_l = (int) strtol(argv[2], NULL, 10);
+    int mem_l = (int) strtol(argv[3], NULL, 10);
+    // Converting to MB
+    mem_l = 1048576*mem_l;
+
+    char* path= argv[1];
+
+    execute_file(path,time_l,mem_l);
+
+
+    return 0;
 }
-
