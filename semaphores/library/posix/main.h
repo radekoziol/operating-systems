a611551f29ce4d6da78b67f1b4584b1df849e838
Radek Kozioł <radekoziol.it@gmail.com>
2018-05-08 09:59:36 +0200

    Basic communication trough shared memory added

---
+++ semaphores/library/posix/main.h
@@ -0,0 +1,11 @@
+//
+// Created by radekkoziol on 05.05.18.
+//
+
+#ifndef SEMAPHORES_MAIN_H
+#define SEMAPHORES_MAIN_H
+
+#define FIFO_NAME "/tmp/myfifao"
+#define MAX_BUF 1024
+
+#endif //SEMAPHORES_MAIN_H
